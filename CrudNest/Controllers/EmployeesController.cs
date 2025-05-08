85fb3b5613061967b78e83aef0daf1cf939feda3
ntima <ntima@MBONGISENINT>
2025-05-08 12:06:14 +0200

    Add project files.

---
+++ CrudNest/Controllers/EmployeesController.cs
@@ -0,0 +1,100 @@
+﻿using CrudNest.Data;
+using CrudNest.Models;
+using CrudNest.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudNest.Controllers
+{
+    [Route("api/[controller]")] //https:/localhost:7***/api/Employees
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext)//Constructor injection
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+           var allEmployees= dbContext.Employee.ToList();
+
+            return Ok(allEmployees);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeebyId(Guid id) 
+        {
+          var employee = dbContext.Employee.Find(id);
+
+            if (employee == null) 
+            {
+                return NotFound("Employee not Found");
+            }
+
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto) //I am using Dto because of separation of concerns(reusable)
+        {
+            var employeeEntity = new Employee()
+            {
+                Email = addEmployeeDto.Email,
+                Name = addEmployeeDto.Name,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary,
+            };
+
+
+            dbContext.Employee.Add(employeeEntity);
+            dbContext.SaveChanges();
+
+            return Ok(employeeEntity);
+            
+
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateEmployee(Guid id , UpdateEmployeeDto updateEmployeeDto)
+        {
+            var employee = dbContext.Employee.Find(id);
+            if (employee == null)
+            {
+                return NotFound("Employee not Found");
+            }
+
+            employee.Name = updateEmployeeDto.Name; //i am updating the propeties here 
+            employee.Email = updateEmployeeDto.Email;
+            employee.Phone = updateEmployeeDto.Phone;
+            employee.Salary = updateEmployeeDto.Salary;
+
+            dbContext.SaveChanges();
+
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+
+        public IActionResult DeleteEmployee(Guid id) 
+        {
+            var employee = dbContext.Employee.Find(id);
+            if (employee == null)
+            {
+                return NotFound("Employee not Found");
+            }
+
+            dbContext.Employee.Remove(employee);
+            dbContext.SaveChanges();
+
+            return Ok("Employee deleted successfully");
+
+        }
+    }
+}
---
+++ CrudNest/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CrudNest.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudNest.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+      
+
+        public DbSet<Employee> Employee{ get; set; }
+    }
+}
